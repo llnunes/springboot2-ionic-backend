d5680325bf05d856d47892b1813aecafdd5d84cf
Lucas L <lucas.lopesdf.gmail.com>
2020-06-22 11:58:32 -0300

    Criando profile de test

---
+++ src/main/java/com/llnunes/cursomc/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.llnunes.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.llnunes.cursomc.resources.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService service;
+
+	@Bean
+	public boolean createTestDatabase() throws ParseException {
+		return service.createTestDatabase();
+	}
+}
